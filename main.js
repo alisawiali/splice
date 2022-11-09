fdf92c96fad8d6ccfa5ef3d6ec4c86a9a90f9512
iimad <alisawiali70@gmail.com>
2022-11-09 18:25:05 +0100

    splice

---
+++ main.js
@@ -0,0 +1,7 @@
+let names =['Ali', 'Omer', 'Imad','Amer','jaml']
+
+names.splice(0,1); // hier löschen die erste name
+
+names.splice(0,1,'Alla'); // hier löschen und ersetzen eine neue name
+console.log(names)
+
